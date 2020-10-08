3629565eea2dbef5879db9dfc6dda249612deef9
LorenVarndell <94055@farnborough.ac.uk>
2020-10-08 23:24:36 +0100

    Adding some JS get request and adding a basic HTML Webpage, not fully working

---
+++ resources/home.js
@@ -0,0 +1,26 @@
+function pageLoad() {
+
+    document.getElementById("getSession").addEventListener("click", getSessionCode);
+}
+
+
+function getSessionCode(){
+    console.log("Fetching session code");
+
+    const SessionID = document.getElementById("SessionID").value;
+    const url = "/Session/SessionID/";
+
+    fetch(url + SessionID, {
+        method: "GET",
+    }).then(response => {
+        return response.json();                 //return response to JSON
+    }).then(response => {
+        if (response.hasOwnProperty("Error")) { //checks if response from server has a key "Error"
+            alert(JSON.stringify(response));    // if it does, convert JSON object to string and alert
+        } else {
+            //this function will create an HTML table of the data (as we
+            // did in lesson 2
+        }
+    });
+
+}
