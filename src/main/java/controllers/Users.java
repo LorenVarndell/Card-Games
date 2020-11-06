674f15fbfadaf340cc9cb19a1696f4ad6fd98460
LorenVarndell <94055@farnborough.ac.uk>
2020-11-06 16:07:46 +0000

    Added API for users and local storage for UserID for the Users, adding user numbers to the data base. Created a few more basic webpages.

---
+++ src/main/java/controllers/Users.java
@@ -0,0 +1,45 @@
+package controllers;
+
+import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.json.simple.JSONObject;
+import server.Main;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@Path("Users/")
+@Consumes(MediaType.MULTIPART_FORM_DATA)
+@Produces(MediaType.APPLICATION_JSON)
+
+
+
+public class Users {
+    @POST
+    @Path("add")
+    public String SessionAdd() {
+        int random_int = (int) (Math.random() * (9999999 - 999999 + 1) + 999999);
+        // String random_intTotal = Integer.toString(random_int) + "" + Integer.toString(random_int2);
+        //System.out.println(random_intTotal);
+        System.out.println(random_int);
+        System.out.println("Invoked Blackjack.SessionIDAdd()");
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("INSERT INTO Users (UserID) VALUES (?)");
+            ps.setInt(1, (int) random_int);
+            JSONObject response = new JSONObject();
+            response.put("UserID", (int) random_int);
+
+            ps.execute();
+            return response.toString();
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to create new item, please see server console for more info.\"}";
+        }
+
+
+    }
+}
