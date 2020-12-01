7dd7a01405aaf1bee11999640639a33e990e8f39
LorenVarndell <94055@farnborough.ac.uk>
2020-12-01 00:05:00 +0000

    experimenting with websockets

---
+++ src/main/java/controllers/MyServlet.java
@@ -0,0 +1,26 @@
+
+package controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
+import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
+
+@WebServlet(name = "WebSocket Servlet", urlPatterns = { "/blackjack" })
+public class MyServlet extends WebSocketServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println("HTTP GET method not implemented.");
+    }
+
+    @Override
+    public void configure(WebSocketServletFactory factory) {
+        factory.getPolicy().setIdleTimeout(10000);
+        factory.register(BlackjackSocket.class);
+    }
+}
